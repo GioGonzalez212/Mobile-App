98bb5110d8b60936099f4ac08e0b9d9d644fe623
GioGonzalez212 <Onanaki123@gmail.com>
2021-12-05 01:15:10 -0500

    Firebase + account sign-in setup layouts

--- Final Project/app/src/main/java/com/example/finalproject/MainActivity.java
+++ Final Project/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,14 +1,169 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 100;
+    private GoogleSignInClient googleSignInClient;
+
+    private FirebaseAuth firebaseAuth;
+
+    private static final String TAG = "GOOGLE_SIGN_IN_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set content for activity layout
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_signin);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener()  {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_signin:
+                        Toast.makeText(MainActivity.this, "Account", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_stocks:
+                        Toast.makeText(MainActivity.this, "Please Sign In to view.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_favorites:
+                        Toast.makeText(MainActivity.this, "Please Sign In to view.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_news:
+                        Toast.makeText(MainActivity.this, "Please Sign In to view.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        //Configure the Google Signin
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        SignInButton googleSignInBtn = (SignInButton) findViewById(R.id.GoogleSignInBtn);
+
+        //Google SignInButton: Click to begin Google Signin
+        googleSignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //begin google sign in
+                Log.d(TAG,"onClick: begin Google SignIn");
+                Intent intent = googleSignInClient.getSignInIntent();
+                startActivityForResult(intent,RC_SIGN_IN);
+            }
+        });
+    }//end of OnCreate
+
+    private void checkUser() {
+        //if user is already signed in then go to profile activity
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null){
+            Log.d(TAG, "checkUser: Already Logged in");
+            startActivity(new Intent(this,ProfileActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            Log.d(TAG,"onActivityResult: Google Signin intent result");
+            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                //google sign in success, now auth with firebase
+                GoogleSignInAccount account = accountTask.getResult(ApiException.class);
+                firebaseAuthWithGoogleAccount(account);
+
+
+            }
+            catch (Exception e){
+                //failed google sign in
+                Log.d(TAG,"onActivityResult: "+e.getMessage());
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogleAccount(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogleAccount: begin firebase auth with google account");
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(@NonNull AuthResult authResult) {
+                        //login success
+                        Log.d(TAG, "onSuccess: Logged In");
+
+                        //get logged in user
+                        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                        //get user info
+                        String uid = firebaseUser.getUid();
+                        String email = firebaseUser.getEmail();
+
+                        Log.d(TAG, "onSuccess: Email: "+email);
+                        Log.d(TAG, "onSuccess: UID: "+uid);
+
+                        //check if user is new or existing
+                        if(authResult.getAdditionalUserInfo().isNewUser()){
+                            //user is new - Account Created
+                            Log.d(TAG, "onSuccess: Account Created...\n"+email);
+                            Toast.makeText(MainActivity.this, "Account Created...\n"+email, Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Log.d(TAG, "onSuccess: Existing user...\n"+email);
+                            Toast.makeText(MainActivity.this, "Existing user...\n"+email, Toast.LENGTH_SHORT).show();
+                        }
+
+                        //start profile activity
+                        startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //login failed
+                        Log.d(TAG, "onFailure: Log In failed "+e.getMessage());
+                    }
+                });
+
     }
 }
---
+++ Final Project/app/src/main/java/com/example/finalproject/ProfileActivity.java
@@ -0,0 +1,110 @@
+package com.example.finalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_main);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.action_signin);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener()  {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_signin:
+                        Toast.makeText(ProfileActivity.this, "Account", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_stocks:
+                        Intent stocksIntent =  new Intent( ProfileActivity.this, StocksActivity.class);
+                        startActivity(stocksIntent);
+                        break;
+                    case R.id.action_favorites:
+                        Toast.makeText(ProfileActivity.this, "Favorites", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_news:
+                        Toast.makeText(ProfileActivity.this, "News", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        Button logoutBtn = (Button) findViewById(R.id.LogoutBtn);
+
+        //handle click, logout
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+                checkUser();
+            }
+        });
+
+    }
+
+    private void checkUser() {
+        TextView emailTv = (TextView) findViewById(R.id.EmailTv);
+        TextView nameTv = (TextView) findViewById(R.id.NameTv);
+        ImageView imageTv = (ImageView) findViewById(R.id.ImageTv);
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser == null){
+            //user not logged in
+            startActivity(new Intent(this,MainActivity.class));
+            finish();
+        }
+        else{
+            //user logged in
+            //get user info
+            String email = firebaseUser.getEmail();
+            String name = firebaseUser.getDisplayName();
+            Uri image = firebaseUser.getPhotoUrl();
+            //set email
+            Picasso.get().load(image).resize(200, 200).into(imageTv);
+            nameTv.setText(name);
+            emailTv.setText(email);
+        }
+    }
+
+    private void signOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //user is now signed out
+                        startActivity(new Intent(ProfileActivity.this,MainActivity.class));
+                        finish();
+                    }
+                });
+    }
+}
